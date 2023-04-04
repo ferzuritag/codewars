36c336529f7df2f7dd8b688aa3569e9fd9431126
= <=>
2023-04-04 15:11:57 -0600

    finished

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,50 @@
-window.addEventListener('load', () => {
-    const hero = document.querySelector('#hero');
+var teleportAudio = new Audio('../assets/teleport.mp3')
 
-    const heroObserver = new IntersectionObserver((entries) => {
+var dragonBallMusic = new Audio('../assets/dragonball.mp3')
+
+window.addEventListener('load', () => {
+    const separator1 = document.querySelector("#separator-1");
+    const gokuBody = document.querySelector('#goku-body');
+    const sections = document.querySelectorAll('section');
+
+
+    const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            } 
         })
     }, {
+
+        rootMargin: "0px",
+        threshold: 0.40
+    });
+    sections.forEach((el) => {
+        observer.observe(el);
+    });
+
+    const observerGokuAppear = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && !gokuBody.classList.contains('body__goku__image--appear')) {
+                setTimeout(() => {
+                    gokuBody.classList.add('body__goku__image--appear');
+                    teleportAudio.play()
+                }, 1000)
+            }
+        })
+    }, {
+
         rootMargin: "0px",
         threshold: 1.0
     });
-    heroObserver.observe(hero)
+    observerGokuAppear.observe(separator1);
 })
+
+function hiddeModal() {
+    const modal = document.querySelector('#main-modal');
+
+    modal.classList.add('display-none');
+    dragonBallMusic.play()
+}
